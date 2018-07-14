c30720cdb8296ef2834bc73486306a367892a0fe
UndeclaredVariable <jshat1@eq.edu.au>
2018-07-14 14:06:19 +1000

    simplified script

--- myScript.js
+++ myScript.js
@@ -1,6 +1,5 @@
 let nameInput = document.getElementById("name");
-let nameDisp = document.getElementById("nameDisp");
 
 nameInput.onchange = function () {
-  nameDisp.innerHTML = nameInput.value;
+  document.getElementById("nameDisp").innerHTML = this.value;
 };
